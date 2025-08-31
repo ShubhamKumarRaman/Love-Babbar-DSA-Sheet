43dadec2f3af576f52d8eff8a05f898910c24a19
ShubhamKumarRaman <shubham620409@gmail.com>
2025-08-31 10:00:59 +0530

    using negative marking

--- 01Arrays/J01DuplicateNumber.java
+++ 01Arrays/J01DuplicateNumber.java
@@ -15,8 +15,24 @@ public class J01DuplicateNumber {
         return -1;
     }
 
+    // Approach 2: Negative Marking (if modifying array is allowed)
+    public static int usingNegativeMarking(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int index = Math.abs(arr[i]);
+            if (arr[index] < 0) {
+                return index;
+            }
+            arr[index] = -arr[index];
+        }
+        return -1;
+    }
+
     public static void main(String args[]) {
-        int arr[] ={1,3,4,2,2};
-        System.out.println("Duplicate value is : "+usingHashSet(arr));
+        int arr[] = { 1, 3, 4, 2, 2 };
+        System.out.println("Duplicate value is : " + usingHashSet(arr));
+
+        int arr2[] = { 3, 1, 3, 4, 2 };
+        System.out.println("Duplicate value is : " + usingNegativeMarking(arr2));
     }
 }

7a08d52db48b49dba8b499570bbceae73d5024ba
Shubham Kumar <shubham620409@gmail.com>
2026-02-13 09:31:25 +0530

    Create J02ReverseList.java

---
+++ 05LinkedList/J02ReverseList.java
@@ -0,0 +1,47 @@
+
+public class J02ReverseList {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Print list
+    public static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    // Reverse List
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node current = head;
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    public static void main(String args[]) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        printList(head);
+        head = reverseList(head);
+        printList(head);
+    }
+}
